d0f3bc11702afefe39cafb0822b70828eb2a98f4
= <gabriel_rodrigues_perez@hotmail.com>
2020-05-23 00:56:46 -0300

    Updates

---
+++ precache-manifest.84f3e68b8ed4a016ec9fa29b57a3ede4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa49a590479e2ef86735f0a5ec7e0b1b",
+    "url": "/gabriel4420.github.io/gama-git/index.html"
+  },
+  {
+    "revision": "d8902b6ea5ac995ea8d8",
+    "url": "/gabriel4420.github.io/gama-git/static/css/main.8fad9c71.chunk.css"
+  },
+  {
+    "revision": "30b0bf2fdd84a4247fed",
+    "url": "/gabriel4420.github.io/gama-git/static/js/2.0d8995cc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gabriel4420.github.io/gama-git/static/js/2.0d8995cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8902b6ea5ac995ea8d8",
+    "url": "/gabriel4420.github.io/gama-git/static/js/main.3b480dc9.chunk.js"
+  },
+  {
+    "revision": "91c73f515e9c08e82c61",
+    "url": "/gabriel4420.github.io/gama-git/static/js/runtime-main.9d06f72b.js"
+  }
+]);
